77676ff9b86b29174731a5dfe1c81621552662b1
HiAndr31 <168109808+HiAndr31@users.noreply.github.com>
2024-04-25 18:59:06 +0300

    Create Ping-Pong.py

---
+++ Ping-Pong.py
@@ -0,0 +1,9 @@
+back = (200,255,255)
+win_width = 600
+win_height = 500
+window= display.set_mode((win_width,win_height)) 
+window.fill(back)
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
