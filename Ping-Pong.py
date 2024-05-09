6d7072ce3039a2404fcbcdae7099a0217f9f23c9
HiAndr31 <168109808+HiAndr31@users.noreply.github.com>
2024-05-09 18:35:33 +0300

    Update Ping-Pong.py
    Bug Fix

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -35,7 +35,7 @@ FPS = 60
 
 racket1 = Player('racket.jpg',30,200,4,50,150)
 racket2= Player('racket.jpg',520,200,4,50,150)
-ball=GameSprite('tennis_ball.jpg',200,200,4,50,50)
+ball=GameSprite('tennis_ball.jpg',300,250,4,50,50)
 font.init()
 font = font.Font(None,35)
 lose1 = font.render('PLAYER 1 LOSE!',True, (180,0,0))
@@ -62,13 +62,11 @@ while game:
             speed_y *= -1
         if ball.rect.x<0:
             finish = True
-            window.blit(lose2,(200,200))
-            game_over = True
-        if ball.rect.y > win_height+50 or ball.rect.y <0:
-            speed_y *= -1
-        if ball.rect.x>550:
-            finish = True
             window.blit(lose1,(200,200))
+            game_over = True
+        if ball.rect.x>win_width-50:
+            finish = True
+            window.blit(lose2,(200,200))
             game_over = True
         racket1.reset()
         racket2.reset()
